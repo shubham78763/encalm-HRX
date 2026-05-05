a7871edffce9f03e84838d7cab57ff7675cadff7
shubham <123365888+shubham78763@users.noreply.github.com>
2026-05-05 11:55:34 +0530

    Update report.routes.ts

--- report.routes.ts
+++ report.routes.ts
@@ -1,39 +1,3 @@
-// import { Router } from "express";
-// import {
-//   getDashboard,
-//   getAttendance,
-//   getPayroll,
-//   exportMonthlyAttendance,
-//   exportLeaveBalance,
-//   exportSalaryRegister,
-//   createAttendance,
-//   createLeave,
-//   createEmployeeProfile,
-//   createSalary,
-//   createTenant,
-//   createUser
-// } from "../controllers/report.controller";
-
-// const router = Router();
-
-// router.get("/dashboard", getDashboard);
-// router.get("/attendance", getAttendance);
-// router.get("/payroll", getPayroll);
-// router.get("/monthly-attendance", exportMonthlyAttendance);
-// router.get("/salary-register", exportSalaryRegister);
-// router.get("/leave-balance", exportLeaveBalance);
-// router.post("/test/attendance", createAttendance);
-// router.post("/test/leave", createLeave);
-// router.post("/test/employee", createEmployeeProfile);
-// router.post("/test/salary", createSalary);
-// router.post("/test/tenant", createTenant);
-// router.post("/test/user", createUser);
-
-
-
-// export default router;
-
-
 import { Router } from "express";
 import {
   getDashboard,
@@ -46,9 +10,9 @@ import {
   createLeave,
   createEmployeeProfile,
   createSalary,
-  createUser,
-  createTenant,
-  createLeaveType
+  // createUser,
+  // createTenant,
+  // createLeaveType
 } from "../controllers/report.controller";
 
 const router = Router();
@@ -80,11 +44,9 @@ router.post("/test/salary", createSalary);
 
 // ...................testing..................
 
-router.post("/test/tenant", createTenant);
-router.post("/test/leavetype", createLeaveType);
+// router.post("/test/tenant", createTenant);
+// router.post("/test/leavetype", createLeaveType);
 
-router.post("/test/user", createUser);
+// router.post("/test/user", createUser);
 
 export default router;
-
-
